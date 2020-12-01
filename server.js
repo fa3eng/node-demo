bce6172eed7031b72a410ddf4278051921748710
meakle <meakle@qq.com>
2020-12-01 11:47:35 +0800

    add

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ var server = http.createServer(function(request, response){
         <link rel="stylesheet" href="/x">
     </header>
     <body>
-        <h1>杨哥真的帅</h1>
+        <h1>标题</h1>
     </body>
 
     `)
